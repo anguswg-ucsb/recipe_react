91b69c36befcc9ca417f6e1440a5fa0f36ae2469
anguswg-ucsb <anguswatters@gmail.com>
2023-11-03 08:56:07 -0700

    async search select components

--- recipe_app/src/App.js
+++ recipe_app/src/App.js
@@ -2,6 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import SearchBar from "./components/search/SearchBar";
 import NavBar from "./components/navbar/NavBar";
+import Select from "react-select";
+import AsyncSelect from "react-select/async";
+import SelectIngredients from "./components/search/SelectIngredients";
+
+const aquaticCreatures = [
+  { label: "Shark", value: "Shark" },
+  { label: "Dolphin", value: "Dolphin" },
+  { label: "Whale", value: "Whale" },
+  { label: "Octopus", value: "Octopus" },
+  { label: "Crab", value: "Crab" },
+  { label: "Lobster", value: "Lobster" },
+];
 
 function App() {
   return (
@@ -12,6 +24,12 @@ function App() {
           <h1>Recipe App</h1>
         </header>
         <SearchBar />
+      </div>
+      <div>
+        <Select options={aquaticCreatures} isMulti />
+      </div>
+      <div>
+        <SelectIngredients />
       </div>
     </>
   );
